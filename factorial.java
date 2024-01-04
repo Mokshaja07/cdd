c6214862fda1bfb9905bfd32c32e6ff6105a7274
Mokshaja07 <153183799+Mokshaja07@users.noreply.github.com>
2024-01-04 16:30:07 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,14 +1,10 @@
-
-
-public class factorial {
-   public static void main(String args[]){
-int num=10;
-long factorial=1;
-for(int i=1;i<=num;++i)
-{
-factorial *=i;
-}
-System.out.println("factorial is"+factorial);
-}
-}
-}
+class FactorialExample{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
