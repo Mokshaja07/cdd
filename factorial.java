df4328d1b19aac7d363491ef1a460eddbb9a6b97
Mokshaja07 <153183799+Mokshaja07@users.noreply.github.com>
2024-01-04 16:17:52 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Factorial {
+public class factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number to find its factorial: ");
